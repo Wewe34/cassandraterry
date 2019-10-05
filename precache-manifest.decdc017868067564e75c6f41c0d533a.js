843800a664fbbe570f7e4c163e769ae1e9bcdcde
Cassandra Terry <cassandra34terry@gmail.com>
2019-10-04 20:16:31 -0500

    Updates

---
+++ precache-manifest.decdc017868067564e75c6f41c0d533a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274b7d904baf386ea52dcf3ba4ba8652",
+    "url": "/cassandraterry/index.html"
+  },
+  {
+    "revision": "de693bb7e9c84ccbba48",
+    "url": "/cassandraterry/static/css/main.bf71db79.chunk.css"
+  },
+  {
+    "revision": "f5959bd03b74c043f0ed",
+    "url": "/cassandraterry/static/js/2.78ff8156.chunk.js"
+  },
+  {
+    "revision": "de693bb7e9c84ccbba48",
+    "url": "/cassandraterry/static/js/main.449c85f3.chunk.js"
+  },
+  {
+    "revision": "678b707f94f129b76a79",
+    "url": "/cassandraterry/static/js/runtime-main.57ddda32.js"
+  }
+]);
